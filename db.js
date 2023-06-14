1185429d83776672a09dc10dad634079200bc822
Joel Cedeno <joelcedeno@Joels-MacBook-Pro.local>
2023-06-13 21:16:09 -0400

    third commit

--- db.js
+++ db.js
@@ -2,24 +2,26 @@
 
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: "postgresql:///biztime"
-});
-
-client.connect();
-
-
-
-// const DB_URI = (process.env.NODE_ENV === "test")
-//   ? "postgresql:///biztime_test"
-//   : "postgresql:///biztime";
-
-// let db = new Client({
-//   connectionString: DB_URI
+// const client = new Client({
+//   connectionString: "postgresql:///biztime"
 // });
 
-// db.connect();
+// client.connect();
 
-// module.exports = db;
 
-module.exports = client;
+
+const DB_URI = (process.env.NODE_ENV === "test")
+  ? "postgresql:///biztime_test"
+  : "postgresql:///biztime";
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+console.log(process.env.NODE_ENV);
+
+module.exports = db;
+
+// module.exports = client;
